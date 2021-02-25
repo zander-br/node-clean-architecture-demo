7807dc1b672b20d12102312dcbed07bb974e275a
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-02-25 19:07:53 -0300

    refactor: change test name

--- tests/entities/employee/name.spec.ts
+++ tests/entities/employee/name.spec.ts
@@ -2,7 +2,7 @@ import { Employee } from '../../../src/entities/employee/employee';
 import { InvalidNameError } from '../../../src/entities/employee/errors/invalid-name';
 import { left } from '../../../src/shared/either';
 
-describe('Employee domain entity', () => {
+describe('Name domain entity', () => {
   test('should not create employee with invalid name (too few characters)', () => {
     const name = 'A';
     const employee = Employee.create({ name });
