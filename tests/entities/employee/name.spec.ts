71d62fca3a2b92632f4e2cb5dbccc7fae17caad4
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-02-25 19:20:12 -0300

    refactor: change tests

--- tests/entities/employee/name.spec.ts
+++ tests/entities/employee/name.spec.ts
@@ -1,27 +1,27 @@
-import { Employee } from '../../../src/entities/employee/employee';
 import { InvalidNameError } from '../../../src/entities/employee/errors/invalid-name';
+import { Name } from '../../../src/entities/employee/name';
 import { left } from '../../../src/shared/either';
 
-describe('Name domain entity', () => {
-  test('should not create employee with invalid name (too few characters)', () => {
-    const name = 'A';
-    const employee = Employee.create({ name });
-    expect(employee).toEqual(left(new InvalidNameError(name)));
+describe('Name domain value object', () => {
+  test('should not create name with invalid parameter (too few characters)', () => {
+    const value = 'A';
+    const name = Name.create(value);
+    expect(name).toEqual(left(new InvalidNameError(value)));
   });
 
-  test('should not create employee with invalid name (too many characters)', () => {
-    let name = '';
+  test('should not create name with invalid parameter (too many characters)', () => {
+    let value = '';
     for (let i = 0; i < 256; i++) {
-      name += 'c';
+      value += 'c';
     }
 
-    const employee = Employee.create({ name });
-    expect(employee).toEqual(left(new InvalidNameError(name)));
+    const name = Name.create(value);
+    expect(name).toEqual(left(new InvalidNameError(value)));
   });
 
-  test('should not create employee with invalid name (only blank spaces)', async () => {
-    const name = '   ';
-    const employee = Employee.create({ name });
-    expect(employee).toEqual(left(new InvalidNameError(name)));
+  test('should not create name with invalid parameter (only blank spaces)', async () => {
+    const value = '   ';
+    const name = Name.create(value);
+    expect(name).toEqual(left(new InvalidNameError(value)));
   });
 });
