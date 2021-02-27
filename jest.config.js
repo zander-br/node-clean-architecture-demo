a52ea9b4e1b1a0dbdc2802a9a7898ebcd22ef8c5
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-02-27 06:16:25 -0300

    chore: add test ci script

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   roots: ['<rootDir>/tests'],
   coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+  ],
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
