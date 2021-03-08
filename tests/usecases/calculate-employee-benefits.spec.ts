b2a646e12f93428fbdc56b5939ba9f5b5077abaf
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-03-08 20:55:28 -0300

    refactor: move sut for factory method

--- tests/usecases/calculate-employee-benefits.spec.ts
+++ tests/usecases/calculate-employee-benefits.spec.ts
@@ -1,12 +1,23 @@
 import CalculateEmployeeBenefits from '@/usecases/calculate-employee-benefits/calculate-employee-benefits';
 import { NotFoundEmployeeError } from '@/usecases/errors/calculate-employee-benefits';
+import { EmployeeRepository } from '@/usecases/ports/employee-repository';
 import InMemoryEmployeeRepository from './ports/in-memory-employee-repository';
+
+type SutTypes = {
+  sut: CalculateEmployeeBenefits;
+  employeeRepositorySpy: EmployeeRepository;
+};
+
+const makeSut = (): SutTypes => {
+  const employeeRepositorySpy = new InMemoryEmployeeRepository();
+  const sut = new CalculateEmployeeBenefits(employeeRepositorySpy);
+  return { sut, employeeRepositorySpy };
+};
 
 describe('Calculate employee benefits use case', () => {
   test('should not calculate benefit when the employee is not found', async () => {
+    const { sut } = makeSut();
     const calculateData = { name: 'not-found-employee', worksDays: 10 };
-    const employeeRepository = new InMemoryEmployeeRepository();
-    const sut = new CalculateEmployeeBenefits(employeeRepository);
     const error = await sut.execute(calculateData);
     expect(error.value).toEqual(new NotFoundEmployeeError(calculateData.name));
     expect(error.isFail()).toBeTruthy();
