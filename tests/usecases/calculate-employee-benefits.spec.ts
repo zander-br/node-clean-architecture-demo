89ccf49218c8c1df71152e0f547a43d15490abe7
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-03-09 05:07:24 -0300

    refactor: move calculate benefits data for make function

--- tests/usecases/calculate-employee-benefits.spec.ts
+++ tests/usecases/calculate-employee-benefits.spec.ts
@@ -2,6 +2,7 @@ import { BenefitsEmptyError } from '@/entities/employee/errors/invalid-employee'
 import CalculateEmployeeBenefits from '@/usecases/calculate-employee-benefits/calculate-employee-benefits';
 import { NotFoundEmployeeError } from '@/usecases/errors/calculate-employee-benefits';
 import { EmployeeRepository } from '@/usecases/ports/employee-repository';
+import { CalculateBenefitsData } from '@/usecases/calculate-employee-benefits/calculate-benefits-data';
 import EmployeeDataBuilder from '../entities/builders/employee-builder';
 import InMemoryEmployeeRepository from './ports/in-memory-employee-repository';
 
@@ -16,10 +17,18 @@ const makeSut = (): SutTypes => {
   return { sut, employeeRepositorySpy };
 };
 
+const makeFakeCalculateData = (): CalculateBenefitsData => ({
+  name: 'Anderson Santos',
+  worksDays: 10,
+});
+
 describe('Calculate employee benefits use case', () => {
   test('should not calculate benefit when the employee is not found', async () => {
     const { sut } = makeSut();
-    const calculateData = { name: 'not-found-employee', worksDays: 10 };
+    const calculateData = {
+      ...makeFakeCalculateData(),
+      name: 'not-found-employee',
+    };
     const error = await sut.execute(calculateData);
     expect(error.value).toEqual(new NotFoundEmployeeError(calculateData.name));
     expect(error.isFail()).toBeTruthy();
@@ -27,7 +36,7 @@ describe('Calculate employee benefits use case', () => {
 
   test('should call findByName with correct value', async () => {
     const { sut, employeeRepositorySpy } = makeSut();
-    const calculateData = { name: 'Anderson Santos', worksDays: 10 };
+    const calculateData = makeFakeCalculateData();
     const findByNameSpy = jest.spyOn(employeeRepositorySpy, 'findByName');
     await sut.execute(calculateData);
 
@@ -36,10 +45,6 @@ describe('Calculate employee benefits use case', () => {
 
   test('should not calculate benefit when the employee benefits is empty', async () => {
     const { sut, employeeRepositorySpy } = makeSut();
-    const calculateData = {
-      name: 'Anderson Santos',
-      worksDays: 10,
-    };
     const mockWithoutBenefits = EmployeeDataBuilder.aEmployee().buildClass();
     jest
       .spyOn(employeeRepositorySpy, 'findByName')
@@ -47,15 +52,14 @@ describe('Calculate employee benefits use case', () => {
         new Promise(resolve => resolve(mockWithoutBenefits)),
       );
 
-    const error = await sut.execute(calculateData);
+    const error = await sut.execute(makeFakeCalculateData());
     expect(error.value).toEqual(new BenefitsEmptyError());
     expect(error.isFail()).toBeTruthy();
   });
 
   test('should return calculate benefits', async () => {
     const { sut } = makeSut();
-    const calculateData = { name: 'Anderson Santos', worksDays: 10 };
-    const response = await sut.execute(calculateData);
+    const response = await sut.execute(makeFakeCalculateData());
 
     expect(response.isSuccess()).toBeTruthy();
     expect(response.value).toHaveLength(1);
