3dfec8590f36cd041dbfb6de3e55e6c2b0a2015d
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-03-12 04:52:37 -0300

    test: should return 400 if CalculateEmployeeBenefits return fail

--- tests/presentation/controllers/calculate-employee-benefits-controller.spec.ts
+++ tests/presentation/controllers/calculate-employee-benefits-controller.spec.ts
@@ -1,6 +1,9 @@
 import { CalculateEmployeeBenefitsController } from '@/presentation/controllers/calculate-employee-benefits-controller';
 import { ICalculateEmployeeBenefits } from '@/usecases/calculate-employee-benefits';
-import { serverError } from '@/presentation/helpers';
+import { badRequest, serverError } from '@/presentation/helpers';
+import { CalculateBenefitsData } from '@/usecases/calculate-employee-benefits/calculate-benefits-data';
+import { fail } from '@/shared/either';
+import { NotFoundEmployeeError } from '@/usecases/errors/calculate-employee-benefits';
 import { CalculateEmployeeBenefitsMock } from '../mocks/mock-calculate-employee-benefits';
 
 type SutTypes = {
@@ -35,6 +38,21 @@ describe('CalculateEmployeeBenefits Controller', () => {
     const request = mockRequest();
     await sut.handle(request);
     expect(calculateEmployeeBenefitsSpy).toHaveBeenCalledWith(request);
+  });
+
+  test('should return 400 if CalculateEmployeeBenefits return fail', async () => {
+    const { sut, calculateEmployeeBenefitsMock } = makeSut();
+    jest
+      .spyOn(calculateEmployeeBenefitsMock, 'execute')
+      .mockImplementationOnce(async ({ name }: CalculateBenefitsData) => {
+        return Promise.resolve(fail(new NotFoundEmployeeError(name)));
+      });
+    const request = mockRequest();
+    const httpResponse = await sut.handle(request);
+
+    expect(httpResponse).toEqual(
+      badRequest(new NotFoundEmployeeError(request.name)),
+    );
   });
 
   test('should return 500 if CalculateEmployeeBenefits throws', async () => {
