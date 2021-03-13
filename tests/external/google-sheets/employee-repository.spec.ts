763767e081f5ea94093dfe4ef424490560a81200
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-03-13 06:25:12 -0300

    test: should return an employee with BILHETE ÚNICO benefit when value greater than zero

--- tests/external/google-sheets/employee-repository.spec.ts
+++ tests/external/google-sheets/employee-repository.spec.ts
@@ -2,41 +2,50 @@ import { GoogleSheetsEmployeeRepository } from '@/external/google-sheets/employe
 
 require('dotenv-flow').config();
 
+const makeSut = (): GoogleSheetsEmployeeRepository => {
+  const spreadSheetId = process.env.SPREAD_SHEET_ID;
+  const credentials = {
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+    private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+  };
+
+  return new GoogleSheetsEmployeeRepository(spreadSheetId, credentials);
+};
+
 describe('GoogleSheetsEmployee Repository', () => {
   test('should return an employee on success', async () => {
-    const spreadSheetId = process.env.SPREAD_SHEET_ID;
-    const credentials = {
-      client_email: process.env.CLIENT_EMAIL,
-      client_id: process.env.CLIENT_ID,
-      private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
-    };
-
-    const employeeRepository = new GoogleSheetsEmployeeRepository(
-      spreadSheetId,
-      credentials,
-    );
+    const sut = makeSut();
     const name = 'VALID EMPLOYEE';
-    const employee = await employeeRepository.findByName(name);
+    const employee = await sut.findByName(name);
 
     expect(employee).toBeTruthy();
     expect(employee.name.value).toBe(name);
   });
 
   test('should return null if findByName fails', async () => {
-    const spreadSheetId = process.env.SPREAD_SHEET_ID;
-    const credentials = {
-      client_email: process.env.CLIENT_EMAIL,
-      client_id: process.env.CLIENT_ID,
-      private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
-    };
-
-    const employeeRepository = new GoogleSheetsEmployeeRepository(
-      spreadSheetId,
-      credentials,
-    );
+    const sut = makeSut();
     const name = 'INVALID EMPLOYEE';
-    const employee = await employeeRepository.findByName(name);
+    const employee = await sut.findByName(name);
 
     expect(employee).toBeFalsy();
+  });
+
+  test('should return an employee with BOM benefit when value greater than zero', async () => {
+    const sut = makeSut();
+    const employee = await sut.findByName('EMPLOYEE WITH BOM');
+
+    expect(employee.hasBenefits()).toBe(true);
+    expect(employee.benefits).toHaveLength(1);
+    expect(employee.benefits[0].name).toEqual('BOM');
+  });
+
+  test('should return an employee with BILHETE ÚNICO benefit when value greater than zero', async () => {
+    const sut = makeSut();
+    const employee = await sut.findByName('EMPLOYEE WITH BILHETE ÚNICO');
+
+    expect(employee.hasBenefits()).toBe(true);
+    expect(employee.benefits).toHaveLength(1);
+    expect(employee.benefits[0].name).toEqual('BILHETE ÚNICO');
   });
 });
