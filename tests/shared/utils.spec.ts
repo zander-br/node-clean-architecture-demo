f1c4f8c548f7c11c89a40c348749b8c913c484cf
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-03-13 05:49:25 -0300

    test: should return numeric value refering to past value changing separators

--- tests/shared/utils.spec.ts
+++ tests/shared/utils.spec.ts
@@ -1,8 +1,13 @@
 import { convertCurrentMoneyInNumber } from '@/shared/utils';
 
 describe('Shared Utils', () => {
-  test('should return zero if current money referes to zero', async () => {
+  test('should return zero if current money referes to zero', () => {
     const result = convertCurrentMoneyInNumber('R$  -');
     expect(result).toBe(0);
+  });
+
+  test('should return numeric value referring to past value changing separators', () => {
+    const result = convertCurrentMoneyInNumber('R$ 1.234,56');
+    expect(result).toBe(1234.56);
   });
 });
