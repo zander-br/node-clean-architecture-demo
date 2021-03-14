962b597bc0a0666e16721ea7ae1d4be360c13261
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-03-14 16:02:30 -0300

    refactor: check return in request test

--- tests/main/routes/calculate-benefits-routes.test.ts
+++ tests/main/routes/calculate-benefits-routes.test.ts
@@ -10,6 +10,24 @@ describe('Register Routes', () => {
         name: 'VALID EMPLOYEE',
         worksDays: 10,
       })
-      .expect(200);
+      .expect(200)
+      .then(response => {
+        expect(response.body).toHaveLength(3);
+        expect(response.body).toEqual([
+          {
+            name: 'VALE ALIMENTAÇÃO',
+            value: 100,
+            type: 'Snack',
+            frequency: 'Monthly',
+          },
+          {
+            name: 'VALE REFEIÇÃO',
+            value: 100,
+            type: 'Food',
+            frequency: 'Daily',
+          },
+          { name: 'BOM', value: 100, type: 'Transport', frequency: 'Daily' },
+        ]);
+      });
   });
 });
